61c6f97ba22fc441767eefc16d4ec20d777c64d3
Ex_person <baranovs11@yandex.ru>
2024-10-08 18:40:30 +0300

    Update version widget.py

--- src/widget.py
+++ src/widget.py
@@ -1,17 +1,24 @@
-import datetime
-
-from masks import get_mask_account
-from masks import get_mask_card_number
+from datetime import datetime
 
 
 def mask_account_card(input_information: str) -> str:
-    numbers = ''.join([str(num) for num in input_information if num.isdigit()])
+    """Функция маскирует данные карты, либо счета"""
+    numbers = "".join([str(num) for num in input_information if num.isdigit()])
+    string = " ".join([i for i in input_information.split(" ") if i.isalpha()])
     if len(numbers) == 20:
-        return get_mask_account(numbers)
+        from src.masks import get_mask_account
+
+        return f"{string} {get_mask_account(numbers)}"
     elif len(numbers) == 16:
-        return get_mask_card_number(numbers)
+        from src.masks import get_mask_card_number
+
+        return f"{string} {get_mask_card_number(numbers)}"
+    else:
+        return "Введены некорректные данные, попробуйте снова."
 
 
-def get_date(data_time: str) -> str:
-    date = data_time[0:10]
-    return f'{date[-2:]}:{date[5:7]}:{date[0:4]}'
+def get_date(date_time: str) -> str:
+    """Функция преобразовывает принимаемую дату в формат "ДД.ММ.ГГГГ" """
+    date = datetime.strptime(date_time, "%Y-%m-%dT%H:%M:%S.%f")
+    new_date = date.strftime("%d.%m.%Y")
+    return new_date
