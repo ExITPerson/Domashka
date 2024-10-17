6d36c75a52a3f7d6e1a3fa29c1aabfbcc04cc1de
Ex_person <baranovs11@yandex.ru>
2024-10-17 15:00:53 +0300

    Add generators and checking the correctness of the program

--- src/masks.py
+++ src/masks.py
@@ -3,16 +3,16 @@ from typing import Union
 
 def get_mask_card_number(card_number: Union[str, int]) -> str:
     """Функция выводит номер карты с замаскированными элементами"""
-    if len(str(card_number)) == 16:
+    if len(str(card_number)) != 16:
+        return "Номер карты не соответствует формату (16 цифр)"
+    elif len(str(card_number)) == 16:
         mask_card = f"{str(card_number)[0:6]}******{str(card_number)[-4:]}"
         return f"{mask_card[0:4]} {mask_card[4:8]} {mask_card[8:12]} {mask_card[-4:]}"
-    else:
-        return "Номер карты введен не корректно, попробуйте еще."
 
 
 def get_mask_account(card_number: Union[str, int]) -> str:
     """Функция выводит последние 6 элементов номера карты с маскировкой первых двух"""
-    if len(str(card_number)) == 20:
+    if len(str(card_number)) != 20:
+        return "Номер карты не соответствует формату (16 цифр)"
+    elif len(str(card_number)) == 20:
         return f"**{str(card_number)[-4:]}"
-    else:
-        return "Номер карты введен не корректно, попробуйте еще."
--- src/widget.py
+++ src/widget.py
@@ -3,22 +3,31 @@ from datetime import datetime
 
 def mask_account_card(input_information: str) -> str:
     """Функция маскирует данные карты, либо счета"""
-    numbers = "".join([str(num) for num in input_information if num.isdigit()])
-    string = " ".join([i for i in input_information.split(" ") if i.isalpha()])
-    if len(numbers) == 20:
-        from src.masks import get_mask_account
+    try:
+        numbers = "".join([str(num) for num in input_information if num.isdigit()])
+        string = " ".join([i for i in input_information.split(" ") if i.isalpha()])
+        if len(numbers) and len(string):
+            if len(numbers) == 20:
+                from src.masks import get_mask_account
 
-        return f"{string} {get_mask_account(numbers)}"
-    elif len(numbers) == 16:
-        from src.masks import get_mask_card_number
+                return f"{string} {get_mask_account(numbers)}"
+            elif len(numbers) == 16:
+                from src.masks import get_mask_card_number
 
-        return f"{string} {get_mask_card_number(numbers)}"
-    else:
-        return "Введены некорректные данные, попробуйте снова."
+                return f"{string} {get_mask_card_number(numbers)}"
+            else:
+                raise ValueError("Введены некорректные данные, попробуйте снова.")
+        else:
+            raise ValueError("Введены некорректные данные, попробуйте снова.")
+    except:
+        raise ValueError("Введены некорректные данные, попробуйте снова.")
 
 
 def get_date(date_time: str) -> str:
     """Функция преобразовывает принимаемую дату в формат "ДД.ММ.ГГГГ" """
-    date = datetime.strptime(date_time, "%Y-%m-%dT%H:%M:%S.%f")
-    new_date = date.strftime("%d.%m.%Y")
-    return new_date
+    try:
+        date = datetime.strptime(date_time, "%Y-%m-%dT%H:%M:%S.%f")
+        new_date = date.strftime("%d.%m.%Y")
+        return new_date
+    except ValueError:
+        return "Не корректный формат даты"
