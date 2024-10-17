c30d8a16480bd683e038d8924f4cf6ee2aed3305
Ex_person <baranovs11@yandex.ru>
2024-10-17 14:05:05 +0300

    Add processing

--- src/Task_2.py
+++
@@ -1,3 +0,0 @@
-def identification_of_a_palindrome(lst: list[int]) -> list[int]:
-    """Функция, которая находит палидром из принимаемого списка"""
-    return [int(element) for element in lst if str(element) == str(element)[::-1]]
